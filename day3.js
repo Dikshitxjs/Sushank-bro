1abe4083ea84225ab64ad207d91d15f3c80665bc
Dikshit Phuyal <dikshit.phuyals24@cps.edu.np>
2025-12-15 08:25:35 +0545

    something about object

--- day3.js
+++ day3.js
@@ -1,12 +1,42 @@
 // spread operator ... 
 
-    const arr1=[ 1,2,3];
-    const arr2=[ 1,2,3];
-    const mergedArr=[...arr1,...arr2];
-    console.log(mergedArr);
+    // const a=[ 1,2,3,4];
+    // const b=[ 1,2,3,4];
+    // const c=[ 1,2,3,4];
+    // const d=[ 1,2,3,4];
+    // const mergedArr=[...arr1,...arr2,...arr3,...arr4];
+    // console.log(mergedArr);
     
-    const mergedArray = (a,b) => {
-        const result = [...a,...b]
-        return result;
-    };
-    console.log(mergedArray(arr1,arr2));
+    // const mergedArray = (a,b) => {
+    //     const result = [...a,...b]
+    //     return result;
+    // };
+    // console.log(mergedArray(arr1,arr2));
+
+    // const mergedArray =(a,b,c,d)=> [...a , ... b,...c,...d];
+    // console.log(mergedArray(a,b,c,d));
+
+
+
+    // Spread operator in objects
+
+    // const newPerson = {
+    //     ...personalbar,
+    //     age : 21,
+    // };
+    //     const newPerson = {
+    //     ...personalbar,
+    //     age : 21,
+    // };
+    // in this case as age 21 is printed not like python value is replaced 
+
+
+    const personDetail = (name, age)=>{
+        const person ={
+            name : "sushank",
+            age: 0
+        }
+      return person;
+    }    
+    console.log(personalDetail(dikshit , 20));
+
