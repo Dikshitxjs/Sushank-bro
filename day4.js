acf4c5813ca9a78dd14f5618166d9dedfd303a87
Dikshit Phuyal <dikshit.phuyals24@cps.edu.np>
2025-12-21 09:09:39 +0545

    day 4

---
+++ day4.js
@@ -0,0 +1,26 @@
+// const numbers = [1,2,3,4,5]
+// const loopnumbers=() =>{
+//     numbers.map((num)=>{
+//         if (num%2===0){
+//             console.log(num)}
+        
+      
+//     })
+// }
+// loopnumbers()
+
+// const something = [1,2,3,4,5]
+// const loopsomething = ()=>{
+//     something.filter (ok)=> {
+//         if (ok/2) {
+//             console.log (ok)
+//         }
+//     }
+// }
+// loopsomething ()
+
+const numbers = [1, 2, 3, 4, 2, 5];
+
+const result = numbers.filter(num => num !== 5);
+
+console.log(result);
